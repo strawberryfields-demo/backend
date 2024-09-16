8e302dad89e0aae39fbacfbbb57099feaa3cd227
nakyoung98 <jay3925@cau.ac.kr>
2024-09-17 01:01:24 +0900

    Chore: env 설정

--- strawberry_be/strawberry_be/settings.py
+++ strawberry_be/strawberry_be/settings.py
@@ -11,10 +11,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# env 관련 옵션
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '../../.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
