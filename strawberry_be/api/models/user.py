156702e6d9aeb4cb7dd564793beee1a73cd08ff7
nakyoung98 <jay3925@cau.ac.kr>
2024-09-18 02:58:46 +0900

    Fix: User 모델 재정의 및 UserManager 구현

--- strawberry_be/api/models/user.py
+++ strawberry_be/api/models/user.py
@@ -1,14 +1,55 @@
-from django.contrib.auth.models import AbstractUser
+import uuid
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from api.models import error
 
 
-class User(AbstractUser):
-    # 데이터베이스에 저장될 실제 값을 문자열로 정의
-    COMPOSER = 'composer'
-    AGENCY = 'agency'
-    USER_TYPE_CHOICES = [
-        (COMPOSER, 'Composer'),
-        (AGENCY, 'Agency'),
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(error.USER_MODEL_ERRORS["email"]["required"])
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    objects = UserManager()
+
+    # 작곡가 보안을 위해 UUID 사용
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.EmailField(
+        max_length=25,
+    )
+    phone = PhoneNumberField(blank=True)
+
+    COMPOSER = "Composer"
+    AGENCY = "Agency"
+
+    USER_TYPE_CHOICES = [(COMPOSER, COMPOSER), (AGENCY, AGENCY)]
+    user_type = models.CharField(
+        max_length=10, choices=USER_TYPE_CHOICES, default=COMPOSER
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+        "phone",
+        "user_type",
     ]
-    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
-    bio = models.TextField(blank=True)
