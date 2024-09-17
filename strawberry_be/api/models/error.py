ab76500048e08410ff61b70c7548e18785aaee39
nakyoung98 <jay3925@cau.ac.kr>
2024-09-18 02:55:28 +0900

    Chore: User 모델 ERROR 메세지 정의

---
+++ strawberry_be/api/models/error.py
@@ -0,0 +1,11 @@
+USER_MODEL_ERRORS = {
+    "email": {
+        "required": {"이메일": "이메일을 입력해주세요."},
+    },
+    "username": {
+        "required": {"이름": "이름을 입력해주세요."},
+    },
+    "phone": {
+        "required": {"전화번호": "전화번호를 입력해주세요."},
+    },
+}
