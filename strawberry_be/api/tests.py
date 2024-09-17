966f162b484e96d9fb24506dbd1c2021ae4fa05a
nakyoung98 <jay3925@cau.ac.kr>
2024-09-18 02:59:27 +0900

    Feat: UserManagerTests 구현
    create_user 성공 테스트

--- strawberry_be/api/tests.py
+++ strawberry_be/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
+class UserManagerTests(TestCase):
+
+    def setUp(self):
+        self.User = get_user_model()
+
+    def test_create_user(self):
+        user = self.User.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="password123",
+            phone="+821012345678",
+            user_type="Composer",
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.check_password("password123"))
