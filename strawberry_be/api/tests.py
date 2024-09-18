07a7b6a7dd3dfabd0a0a0cf480b762ba45099040
nakyoung98 <jay3925@cau.ac.kr>
2024-09-18 12:51:12 +0900

    Feat: UserSerializerTests 구현

--- strawberry_be/api/tests.py
+++ strawberry_be/api/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
+
+
+from api.serializers.error import USER_SERIALIZE_ERRORS
+from .serializers import UserSerializer
 
 
 # Create your tests here.
@@ -31,4 +36,68 @@ class UserManagerTests(TestCase):
             user_type="Composer",
         )
         self.assertNotEqual("password123", user.password)
+
+
+class UserSerializerTest(TestCase):
+
+    def setUp(self):
+        # 테스트용 사용자 생성
+        User = get_user_model()
+
+        self.existing_user = User.objects.create_user(
+            username="existinguser",
+            phone="+821012345678",
+            email="existing@example.com",
+            password="existingpassword",
+            user_type="Composer",
+        )
+
+    # 중복 이메일로 인한 유효성 검사 실패 테스트
+    def test_email_unique_validation(self):
+        data = {
+            "username": "newuser",
+            "phone": "+821012345678",
+            "email": "existing@example.com",
+            "password": "newpassword",
+            "user_type": "Composer",
+        }
+        serializer = UserSerializer(data=data)
+        with self.assertRaises(ValidationError) as context:
+            serializer.is_valid(raise_exception=True)
+
+        self.assertIn("email", context.exception.detail)
+        self.assertEqual(
+            context.exception.detail["email"],
+            USER_SERIALIZE_ERRORS["email"]["unique"],
+        )
+
+    # 불충분한 비밀번호로 인한 사용자 생성 테스트
+    def test_password_invalidation(self):
+        data = {
+            "username": "newuser",
+            "email": "newuser@example.com",
+            "phone": "+821012345678",
+            "password": "password",
+            "user_type": "Composer",
+        }
+        serializer = UserSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+    # 유효한 데이터로 인한 사용자 생성 테스트
+    def test_user_creation(self):
+        data = {
+            "username": "newuser",
+            "email": "newuser@example.com",
+            "phone": "+821012345678",
+            "password": "adfdqererad!",
+            "user_type": "Composer",
+        }
+        serializer = UserSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+        user = serializer.save()
+        self.assertEqual(user.username, data["username"])
+        self.assertEqual(user.email, data["email"])
+        self.assertTrue(user.check_password(data["password"]))
+        self.assertEqual(user.user_type, data["user_type"])
 
