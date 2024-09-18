693690fb149eac943af05463de532f8fb40356ba
nakyoung98 <jay3925@cau.ac.kr>
2024-09-18 12:47:44 +0900

    Chore: User 시리얼라이저 ERROR 메세지 정의

---
+++ strawberry_be/api/serializers/error.py
@@ -0,0 +1,11 @@
+USER_SERIALIZE_ERRORS = {
+    "password": {},
+    "email": {
+        "required": {"이메일": "이메일을 입력해주세요."},
+        "unique": {"이메일": "이미 가입한 이메일입니다."},
+    },
+    "username": {
+        "required": {"이름": "이름을 입력해주세요."},
+    },
+    "phone": {"required": {"전화번호": "전화번호를 입력해주세요."}},
+}
