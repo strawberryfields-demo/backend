82690dd8401f67b530f39d707cc5e2de4fb94cf7
nakyoung98 <jay3925@cau.ac.kr>
2024-09-19 01:23:26 +0900

    Feat: sign-in api 구현

--- strawberry_be/api/views/user_view.py
+++ strawberry_be/api/views/user_view.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 
+from api.serializers.signin_serializer import SigninSerializer
 from api.serializers.user_serializer import UserSerializer
 
 
@@ -19,3 +20,16 @@ class UserView(viewsets.ViewSet):
         user_serializer.save()
 
         return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=False,
+        methods=["POST"],
+        url_path="sign-in",
+        permission_classes=[AllowAny],
+    )
+    def sign_in(self, request):
+        signin_serializer = SigninSerializer(data=request.data)
+        if signin_serializer.is_valid(raise_exception=True):
+            user = signin_serializer.validated_data.get("user")
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
