39202b91572febd2c00b16ad4b8b9ab6e453f1d5
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 03:28:43 +0900

    Feat: CORS 정책 추가

--- strawberry_be/strawberry_be/settings.py
+++ strawberry_be/strawberry_be/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger',
 
+    # CORS 관련
+    "corsheaders",
     # model 관련 library
     "phonenumber_field",  # 전화번호 관련
     # app
@@ -58,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -166,3 +169,6 @@ REST_FRAMEWORK = {
 
 # auth 관련 기본 model 설정
 AUTH_USER_MODEL = 'api.User'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
