a6535e655ca54bfc51df317fafe8d7d858f7f83f
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 02:23:19 +0900

    Fix: BASE_DIR 경로 수정

--- strawberry_be/strawberry_be/settings.py
+++ strawberry_be/strawberry_be/settings.py
@@ -15,7 +15,7 @@ import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # env 관련 옵션
 
