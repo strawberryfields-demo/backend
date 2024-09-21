5be563760170901bcdf612cca125cb22943e4878
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 09:41:45 +0900

    Feat: jwt_middleware 삭제
    permissions와 일부 중복되어 불필요

--- strawberry_be/strawberry_be/settings.py
+++ strawberry_be/strawberry_be/settings.py
@@ -68,7 +68,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "api.middleware.jwt_middleware.JWTMiddleware",
 ]
 
 ROOT_URLCONF = "strawberry_be.urls"
