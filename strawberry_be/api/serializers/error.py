9fb82d1136daeca7778c22113fc9aa74eccd4f0f
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 21:07:25 +0900

    Fix: Error key 영어로 변경

--- strawberry_be/api/serializers/error.py
+++ strawberry_be/api/serializers/error.py
@@ -1,18 +1,20 @@
 USER_SERIALIZE_ERRORS = {
     "password": {},
     "email": {
-        "required": {"이메일": "이메일을 입력해주세요."},
-        "unique": {"이메일": "이미 가입한 이메일입니다."},
+        "required": {"email": "이메일을 입력해주세요."},
+        "unique": {"email": "이미 가입한 이메일입니다."},
     },
     "username": {
-        "required": {"이름": "이름을 입력해주세요."},
+        "required": {"username": "이름을 입력해주세요."},
     },
-    "phone": {"required": {"전화번호": "전화번호를 입력해주세요."}},
+    "phone": {"required": {"phone": "전화번호를 입력해주세요."}},
 }
 
 SIGNIN_SERIALIZE_ERRORS = {
-    "email": {"not_found": {"이메일": "존재하지 않는 유저입니다"}},
-    "password": {"match": {"비밀번호": "일치하지 않는 비밀번호입니다."}},
+    "email": {"not_found": {"email": "존재하지 않는 유저입니다"}},
+    "password": {"match": {"password": "일치하지 않는 비밀번호입니다."}},
+}
+
 S3_URL_SERIALIZE_ERRORS = {
     "file": {
         "required": {"file": "파일을 첨부해주세요."},
